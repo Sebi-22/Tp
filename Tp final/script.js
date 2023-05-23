5203c1f9696cf3e16af590c266d82f9be6074298
Sebi-22 <srotbard@hotmail.com>
2023-05-23 11:06:50 -0300

    Cambios

--- Tp final/script.js
+++ Tp final/script.js
@@ -1,39 +1,39 @@
 class Producto {
     constructor(nombre, descripcion, precio) {
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
+      this.nombre = nombre;
+      this.descripcion = descripcion;
+      this.precio = precio;
     }
-}
-
-class LocalIndumentaria {
+  }
+  
+  class LocalIndumentaria {
     constructor() {
-        this.productos = [];
+      this.productos = [];
     }
-
+  
     agregarProducto(producto) {
-        this.productos.push(producto);
-        console.log("Producto agregado: " + producto.nombre);
+      this.productos.push(producto);
+      console.log("Producto agregado: " + producto.nombre);
     }
-
+  
     mostrarProductos() {
-        console.log("Lista de productos:");
-        this.productos.forEach(producto => {
-            console.log(producto.nombre + " - " + producto.precio);
-        });
+      console.log("Lista de productos:");
+      this.productos.forEach((producto) => {
+        console.log(`${producto.nombre} - ${producto.precio}`);
+      });
     }
-}
-
-const local = new LocalIndumentaria();
-
-// Agregar productos
-const producto1 = new Producto("Camisa blanca", "Camisa de manga larga", 1500.0);
-const producto2 = new Producto("Pantalón negro", "Pantalón de tela", 2000.0);
-const producto3 = new Producto("Vestido rojo", "Vestido elegante", 2500.0);
-
-local.agregarProducto(producto1);
-local.agregarProducto(producto2);
-local.agregarProducto(producto3);
-
-// Mostrar lista de productos
-local.mostrarProductos();
+  }
+  
+  const local = new LocalIndumentaria();
+  
+  // Agregar productos
+  const producto1 = new Producto("Camisa blanca", "Camisa manga corta, 1500.0);
+  const producto2 = new Producto("Pantalón azul corto", "Pantalón de tela", 2000.0);
+  const producto3 = new Producto("Vestido rojo", "Vestido elegante", 2500.0);
+  
+  local.agregarProducto(producto1);
+  local.agregarProducto(producto2);
+  local.agregarProducto(producto3);
+  
+  // Mostrar lista de productos
+  local.mostrarProductos();
