c61b5fbbfb19c411bd17c26f3707f3d4afc3291b
Sebi-22 <srotbard@hotmail.com>
2023-06-08 12:21:16 -0300

    Nuevas cosas

---
+++ Tp final/HTML/JS/script.js
@@ -0,0 +1,39 @@
+class Producto {
+    constructor(nombre, descripcion, precio) {
+      this.nombre = nombre;
+      this.descripcion = descripcion;
+      this.precio = precio;
+    }
+  }
+  
+  class LocalIndumentaria {
+    constructor() {
+      this.productos = [];
+    }
+  
+    agregarProducto(producto) {
+      this.productos.push(producto);
+      console.log("Producto agregado: " + producto.nombre);
+    }
+  
+    mostrarProductos() {
+      console.log("Lista de productos:");
+      this.productos.forEach((producto) => {
+        console.log(`${producto.nombre} - ${producto.precio}`);
+      });
+    }
+  }
+  
+  const local = new LocalIndumentaria();
+  
+  // Agregar productos
+  const producto1 = new Producto("Camisa blanca", "Camisa manga corta, 1500.0);
+  const producto2 = new Producto("Pantalón azul corto", "Pantalón de tela", 2000.0);
+  const producto3 = new Producto("Vestido rojo", "Vestido elegante", 2500.0);
+  
+  local.agregarProducto(producto1);
+  local.agregarProducto(producto2);
+  local.agregarProducto(producto3);
+  
+  // Mostrar lista de productos
+  local.mostrarProductos();
