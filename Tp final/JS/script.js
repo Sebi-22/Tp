1d1031915f9ca52ab588fd9ab3f76b74b531d52f
Sebi-22 <srotbard@hotmail.com>
2023-06-12 13:20:52 -0300

    Penultimas modifcaciones

--- Tp final/JS/script.js
+++ Tp final/JS/script.js
@@ -1,39 +1,53 @@
 class Producto {
-    constructor(nombre, descripcion, precio) {
-      this.nombre = nombre;
-      this.descripcion = descripcion;
-      this.precio = precio;
-    }
+  constructor(nombre, descripcion, precio) {
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precio = precio;
   }
-  
-  class LocalIndumentaria {
-    constructor() {
-      this.productos = [];
-    }
-  
-    agregarProducto(producto) {
-      this.productos.push(producto);
-      console.log("Producto agregado: " + producto.nombre);
-    }
-  
-    mostrarProductos() {
-      console.log("Lista de productos:");
-      this.productos.forEach((producto) => {
-        console.log(`${producto.nombre} - ${producto.precio}`);
-      });
-    }
+}
+
+class LocalIndumentaria {
+  constructor() {
+    this.productos = [];
   }
-  
-  const local = new LocalIndumentaria();
-  
-  // Agregar productos
-  const producto1 = new Producto("Camisa blanca", "Camisa manga corta, 1500.0);
-  const producto2 = new Producto("Pantalón azul corto", "Pantalón de tela", 2000.0);
-  const producto3 = new Producto("Vestido rojo", "Vestido elegante", 2500.0);
-  
-  local.agregarProducto(producto1);
-  local.agregarProducto(producto2);
-  local.agregarProducto(producto3);
-  
-  // Mostrar lista de productos
-  local.mostrarProductos();
+
+  agregarProducto(producto) {
+    this.productos.push(producto);
+    console.log("Producto agregado: " + producto.nombre);
+  }
+
+  mostrarProductos() {
+    console.log("Lista de productos:");
+    this.productos.forEach((producto) => {
+      console.log(`${producto.nombre} - ${producto.precio}`);
+    });
+  }
+}
+
+const local = new LocalIndumentaria();
+
+// Agregar productos
+const producto1 = new Producto("Camisa blanca", "Camisa de manga larga", 1500.0);
+const producto2 = new Producto("Pantalón negro", "Pantalón de tela", 2000.0);
+const producto3 = new Producto("Vestido rojo", "Vestido elegante", 2500.0);
+
+local.agregarProducto(producto1);
+local.agregarProducto(producto2);
+local.agregarProducto(producto3);
+
+// Mostrar lista de productos
+local.mostrarProductos();
+
+const botonAviso = document.getElementById("cerrar-aviso");
+const ventanaModal = document.getElementById("ventana-modal");
+const fondoModal = document.getElementById("fondo-modal");
+
+if (!sessionStorage.getItem("ventana-modal-visto")) {
+    ventanaModal.classList.add("activo")
+    fondoModal.classList.add("activo")
+}
+
+botonAviso.addEventListener("click", () => {
+  ventanaModal.classList.remove("activo");
+  sessionStorage.setItem("ventana-modal-visto", true)
+})
