122920939d8bba8384010eff2c10cbe128641e0b
Sebi-22 <srotbard@hotmail.com>
2023-06-15 14:00:11 -0300

    Guardad cambios

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,26 @@
-const open = document.getElementById('open');
-const modal_container = document.getElementById('modal_container');
-const close = document.getElementById('close');
-
-open.addEventListener('click', () => {
-    modal_container.classList.add('show');
-});
-
-close.addEventListener('click', () => {
-    modal_container.classList.remove('show');
-});
 const botonAviso = document.getElementById("cerrar-aviso");
 const ventanaModal = document.getElementById("ventana-modal");
 const fondoModal = document.getElementById("fondo-modal");
 
-
-//*Condicional
-//* ! (si no hay nada guardadao repetir aviso)
-if(!sessionStorage.getItem("ventana-modal-visto")){
-    ventanaModal.classList.add("activo") 
-    fondoModal.classList.add("activo") //*Mostramos la clase de html
+if (!sessionStorage.getItem("ventana-modal-visto")) {
+    ventanaModal.classList.add("activo")
+    fondoModal.classList.add("activo")
 }
-
-botonAviso.addEventListener("click", () =>{ //*Funcion flecha
-    ventanaModal.classList.remove("activo");//*eliminacion de clase en html
-    fondoModal.classList.remove("activo");
-    sessionStorage.setItem("ventana-modal-visto", true)//*Guardamos la informacion y no se repite aviso
+botonAviso.addEventListener("click", () => {
+    ventanaModal.classList.remove("activo");
+    sessionStorage.setItem("ventana-modal-visto", true)
 })
-
+//boton primero
+document.getElementById('btnsaludo').onclick = function () {
+    alert('Saludos, Gracias por visitar StyleHub!');
+}
+//modal segundo
+var modal = document.getElementById("ventanaModal");
+var boton = document.getElementById("abrirModal");
+var span = document.getElementsByClassName("cerrar")[0];
+boton.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+span.addEventListener("click", function () {
+    modal.style.display = "none";
+});
